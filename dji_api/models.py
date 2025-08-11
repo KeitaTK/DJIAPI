40314cfdf4f645b4513427b94fd69cacd3a03c22
KeitaTK <keita17101042@gmail.com>
2025-08-11 15:58:58 +0900

    refresh_token = models.CharField(max_length=255, blank=True)

--- dji_api/models.py
+++ dji_api/models.py
@@ -1,7 +1,7 @@
 # dji_api/models.py
 from django.db import models
 class DJICloudToken(models.Model):
-    access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+    access_token  = models.CharField(max_length=255, blank=True)
+    refresh_token = models.CharField(max_length=255, blank=True)
     expires_at   = models.DateTimeField()
     updated_at   = models.DateTimeField(auto_now=True)
