41f0400cfd4680788566a26e9538cf7cb463ce98
KeitaTK <keita17101042@gmail.com>
2025-08-11 17:01:50 +0900

    無効

--- dji_api/services.py
+++ dji_api/services.py
@@ -2,31 +2,50 @@
 
 import requests
 import urllib3
+import ssl
+from requests.adapters import HTTPAdapter
+from urllib3.util.ssl_ import create_urllib3_context
 from django.utils import timezone
 from .models import DJICloudToken, UserDJICredentials
 
-# 開発環境での警告抑制
+# 警告抑制（開発環境限定）
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 API_BASE = 'https://api.djicloud.com/v1'
 
+class TLS12Adapter(HTTPAdapter):
+    """
+    TLS1.2 を強制使用するための HTTPAdapter
+    """
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = create_urllib3_context()
+        # TLSv1.2 以上のみ許可
+        ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+        kwargs['ssl_context'] = ctx
+        return super().init_poolmanager(*args, **kwargs)
+
+def get_session():
+    """
+    TLS1.2 固定の Session を返す
+    """
+    session = requests.Session()
+    # HTTPS 全般に対して TLS12Adapter を適用
+    session.mount('https://', TLS12Adapter())
+    return session
+
 def refresh_user_token(user):
-    """
-    アクセストークン取得エンドポイント呼び出し
-    SSL 検証を一時的に無効化 (verify=False)
-    """
     creds = user.dji_credentials
     payload = {
         'app_id':     creds.app_id,
         'app_key':    creds.app_key,
         'grant_type': 'client_credentials',
     }
-
-    # verify=False で SSL 検証を無効化
-    res = requests.post(
+    session = get_session()
+    # verify=False も併用して検証を無効化（開発環境限定）
+    res = session.post(
         f'{API_BASE}/oauth/token',
         json=payload,
-        verify=False
+        verify=False,
     )
     res.raise_for_status()
     data = res.json()
@@ -34,30 +53,23 @@ def refresh_user_token(user):
     token_obj, _ = DJICloudToken.objects.get_or_create(id=user.id)
     token_obj.access_token  = data['access_token']
     token_obj.refresh_token = data.get('refresh_token', '')
-    token_obj.expires_at    = timezone.now() + timezone.timedelta(
-        seconds=data.get('expires_in', 0)
-    )
+    token_obj.expires_at    = timezone.now() + timezone.timedelta(seconds=data.get('expires_in', 0))
     token_obj.save()
     return token_obj
 
 def ping_cloud_api(user):
-    """
-    デバイスエンドポイント呼び出し
-    SSL 検証を一時的に無効化 (verify=False)
-    """
     token_obj = refresh_user_token(user)
     headers   = {'Authorization': f'Bearer {token_obj.access_token}'}
-
-    res = requests.get(
+    session = get_session()
+    res = session.get(
         f'{API_BASE}/devices',
         headers=headers,
         timeout=5,
-        verify=False
+        verify=False,
     )
     res.raise_for_status()
-
     return {
         'ping_success': True,
         'ping_error':   None,
-        'expires_at':   token_obj.expires_at
+        'expires_at':   token_obj.expires_at,
     }
